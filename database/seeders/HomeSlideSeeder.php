7f87c6a44a1f73f94f115b1b7ec581886bff00de
Jalelman <64267966+Jalelman@users.noreply.github.com>
2023-01-05 09:42:33 +0100

    seeder final version

--- database/seeders/HomeSlideSeeder.php
+++ database/seeders/HomeSlideSeeder.php
@@ -15,10 +15,10 @@ class HomeSlideSeeder extends Seeder
     public function run(){
         DB::table('home_slides')->insert([
             'id' => '1',
-            'title' => 'Best Coder',
-            'short_title' => 'This is a a basic local website',
-            'video_url' => 'https://www.youtube.com/watch?v=dYZXeShv7nQ',
-            'home_slide' => '',
+            'title' => 'best coder ever',
+            'short_title' => 'this is a simple test of my website that i should take it as project to introduce my knowledge of laravel',
+            'video_url' => 'https://www.youtube.com/watch?v=yGcfXw9sw84&t=179s',
+            'home_slide' => 'upload/home_slide/1754107925302716.jpg',
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,11 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run(){
         DB::table('users')->insert([
-            'name' => 'Jalel',
-            'email' => 'Jalel@gmail.com',
-            'username' => 'jalel',
-            'profile_image' => '',
-            'password' => Hash::make('jalel12345'),
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'username' => 'user',
+            'profile_image' => '202301041500321604533_685975619654398_226767018089136715_n.jpg',
+            'email_verified_at'=>'2023-01-04 14:50:46',
+            'password' => Hash::make('12345678'),
+            'created_at'=>'2023-01-04 14:50:27',
+            'updated_at'=>'2023-01-04 15:00:29',
         ]);
     }
 }
